af2a8a29db6cbdd29196e7bcbbea56e067fc6d22
Michal Dziadkowiec <michal.dziadkowiec123@gmail.com>
2019-04-06 13:38:27 +0200

    added ABOUT US section

--- src/js/components/Map.js
+++ src/js/components/Map.js
@@ -1,6 +1,6 @@
 // import L from 'leaflet'
 
-const odaconnectPLlonLat = [49.9656593, 20.6081992];
+const odaconnectPLlonLat = [49.9655551, 20.6103334];
 const map = L.map('map').setView(odaconnectPLlonLat, 16);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
